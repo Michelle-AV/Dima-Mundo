8112a1eedb363392d3613b39385baf7d10fb4b0a
clxsrdev <chema.pfdez@gmail.com>
2024-06-07 18:07:05 -0600

    Initial Commit

---
+++ Dima Mundo/Dima_MundoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dima_MundoApp.swift
+//  Dima Mundo
+//
+//  Created by Chema Padilla Fdez on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Dima_MundoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
